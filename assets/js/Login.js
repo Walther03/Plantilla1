3c273dbce679c5a63239e9d3dc86496facdc638a
admin <admin@10.0.2.15>
2019-09-02 18:17:01 -0500

    Se agrega jar

---
+++ assets/js/Login.js
@@ -0,0 +1,12 @@
+function getCredenciales()
+						{
+						var User = document.getElementById("User").value;
+						var Pass = document.getElementById("Pass").value;
+						if (User == "TDP2019" && Pass == "1234" ) {
+							   window.location = "TDPAuto.html";
+							}
+							else  {
+							   alert("Credenciales Incorrectas");
+							}
+						
+						}
