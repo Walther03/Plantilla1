6228bdddb18ef571a6e9a336dcb10f3bf3bbbb8f
admin <admin@10.0.2.15>
2019-09-09 15:11:42 -0500

    CAMBIOS

--- assets/js/Login.js
+++ assets/js/Login.js
@@ -1,13 +1,39 @@
-function getCredenciales()
+var Usuario= "0";
+
+function getCredenciales(user,pass)
 						{
-	
-						var User = document.getElementById("User").value;
-						var Pass = document.getElementById("Pass").value;
-						if (User == "TDP2019" && Pass == "1234" ) {
+						User = user;
+						var Pass = pass;
+						if (User == "TDP2019" && Pass == "1234" ) 
+						{
+							alert("Credenciales Correctas");
+							Usuario= "1";
+							
 							   window.location = "TDPAuto.html";
+							   
 							}
 							else  {
 							   alert("Credenciales Incorrectas");
 							}
 						
 						}
+
+
+
+
+
+function sesion()
+{
+
+if (Usuario == "0" ) 
+{
+	alert("Tiene que inicar sesion");
+	window.location = "Login.html";
+	}
+	else  {
+		alert("Inicio Sesion");
+		   window.location = "TDPAuto.html";
+	   
+	}
+
+}
